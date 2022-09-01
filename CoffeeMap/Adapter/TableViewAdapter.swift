9d9f671941bedbdd55e42ac40b725de910dbba8f
KaoWenChung <k022298@gmail.com>
2022-09-01 09:29:52 +0800

    Create TableViewAdapter

--- CoffeeMap/Adapter/TableViewAdapter.swift
+++ CoffeeMap/Adapter/TableViewAdapter.swift
@@ -5,3 +5,16 @@
 //  Created by owenkao on 2022/09/01.
 //
 
+import UIKit
+
+protocol BaseCellRowModel {
+    var cellID: String { get set }
+    var cellAction: ((BaseCellRowModel)->())? { get set }
+}
+
+class TableViewAdapter: NSObject {
+
+    weak var tableView: UITableView?
+
+    var rowModels: [BaseCellRowModel] = []
+}
