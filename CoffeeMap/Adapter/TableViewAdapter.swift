d2c12e476a8a6b7719c937f8ad990b17d548920c
KaoWenChung <k022298@gmail.com>
2022-09-01 09:37:58 +0800

    Define BaseCellView, TableViewAdapter

--- CoffeeMap/Adapter/TableViewAdapter.swift
+++ CoffeeMap/Adapter/TableViewAdapter.swift
@@ -12,9 +12,63 @@ protocol BaseCellRowModel {
     var cellAction: ((BaseCellRowModel)->())? { get set }
 }
 
+protocol BaseCellView: UITableViewCell {
+    func setupCellView(rowModel: BaseCellRowModel)
+}
+
+extension BaseCellView {
+    var cellReuseIdentifier: String {
+        return "\(type(of: self))"
+    }
+}
+
 class TableViewAdapter: NSObject {
 
     weak var tableView: UITableView?
 
-    var rowModels: [BaseCellRowModel] = []
+    var rowModels: [[BaseCellRowModel]] = []
+
+    init(_ tableView: UITableView, cell: BaseCellView) {
+        super.init()
+        self.tableView = tableView
+        self.tableView?.register(UINib(nibName: cell.cellReuseIdentifier, bundle: nil), forCellReuseIdentifier: cell.cellReuseIdentifier)
+        self.tableView?.delegate = self
+        self.tableView?.dataSource = self
+    }
 }
+
+// MARK: - TableViewAdapter UITableViewDataSource
+extension TableViewAdapter: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return rowModels[section].count
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return rowModels.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: rowModels[indexPath.section][indexPath.row].cellID, for: indexPath)
+        
+        if let cell = cell as? BaseCellView {
+            cell.setupCellView(rowModel: rowModels[indexPath.section][indexPath.row])
+        }
+
+        return cell
+    }
+
+}
+
+// MARK: - TableViewAdapter UITableViewDelegate
+extension TableViewAdapter: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let action = rowModels[indexPath.section][indexPath.row].cellAction {
+            action(rowModels[indexPath.section][indexPath.row])
+        }
+    }
+
+}
+
---
+++ CoffeeMapTests/CoffeeMapTests.swift
@@ -0,0 +1,12 @@
+//
+//  CoffeeMapTests.swift
+//  CoffeeMapTests
+//
+//  Created by owenkao on 2022/09/01.
+//
+
+import XCTest
+
+class CoffeeMapTests: XCTestCase {
+
+}
