ecaddaa63e6ee27c4564b5cf4c57eeba093f1b74
KaoWenChung <k022298@gmail.com>
2022-09-01 10:48:24 +0800

    Defined cell list in PlaceSearchViewModel

--- CoffeeMap/ViewModel/PlaceSearchViewModel.swift
+++ CoffeeMap/ViewModel/PlaceSearchViewModel.swift
@@ -7,6 +7,9 @@
 
 import Foundation
 
-class PlaceSearchViewModel {
-//    private(set) var placeList: []
+class PlaceSearchViewModel: BaseViewModel {
+
+    private(set) var placeList: [PlaceSearchCellViewModel] = []
+
+    
 }
