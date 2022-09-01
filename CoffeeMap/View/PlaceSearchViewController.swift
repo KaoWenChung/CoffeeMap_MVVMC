8255bef2e0b9d86e361491a63052440dfc5ca6e5
KaoWenChung <k022298@gmail.com>
2022-09-01 09:29:38 +0800

    Create the PlaceSearchViewModel

--- CoffeeMap/View/PlaceSearchViewController.swift
+++ CoffeeMap/View/PlaceSearchViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 class PlaceSearchViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
+    private var tableViewAdapter: TableViewAdapter?
+    private let viewModel: PlaceSearchViewModel
+
+    init(_ viewModel: PlaceSearchViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        self.viewModel = PlaceSearchViewModel()
+        super.init(coder: coder)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ CoffeeMap/ViewModel/PlaceSearchViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  PlaceSearchViewViewModel.swift
+//  CoffeeMap
+//
+//  Created by owenkao on 2022/09/01.
+//
+
+import Foundation
+
+class PlaceSearchViewModel {
+    
+}
