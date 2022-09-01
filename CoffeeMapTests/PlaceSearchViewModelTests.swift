91029d024d5ac9492355431dd19e0c2c92bf27b4
KaoWenChung <k022298@gmail.com>
2022-09-01 11:55:36 +0800

    Created PlaceSearchViewModelTests, extension XCTestCase loadStub, fetchStubModel for injecting test fixture

---
+++ CoffeeMapTests/PlaceSearchViewModelTests.swift
@@ -0,0 +1,13 @@
+//
+//  PlaceSearchViewModelTests.swift
+//  CoffeeMapTests
+//
+//  Created by owenkao on 2022/09/01.
+//
+
+import XCTest
+@testable import CoffeeMap
+
+class PlaceSearchViewModelTests: XCTestCase {
+    func testgetPlaceListBy_() throws {}
+}
---
+++ CoffeeMapTests/Util/XCTestCase+.swift
@@ -0,0 +1,26 @@
+//
+//  XCTestCase+.swift
+//  CoffeeMapTests
+//
+//  Created by owenkao on 2022/09/01.
+//
+
+import XCTest
+
+extension XCTestCase {
+
+    func loadStub(name aName: String, extension aExtension: String)-> Data {
+        let bundle = Bundle(for: type(of: self))
+        let url = bundle.url(forResource: aName, withExtension: aExtension)
+      return try! Data(contentsOf: url!)
+    }
+
+    func fetchStubModel<T:Decodable>(fileName aFileName: String) throws -> T {
+        let data = loadStub(name: aFileName, extension: "json")
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .secondsSince1970
+        let decodableData = try decoder.decode(T.self, from: data)
+        return decodableData
+    }
+
+}
