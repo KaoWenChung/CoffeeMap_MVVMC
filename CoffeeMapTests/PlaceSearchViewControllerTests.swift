13e973828af7be961ba1b0d5f2c8639ba4a015b7
KaoWenChung <k022298@gmail.com>
2022-09-02 11:03:27 +0800

    Defined method makeSUT in PlaceSearchViewControllerTests, created test of tableView UI

--- CoffeeMapTests/PlaceSearchViewControllerTests.swift
+++ CoffeeMapTests/PlaceSearchViewControllerTests.swift
@@ -12,10 +12,26 @@ import CoreLocation
 class PlaceSearchViewControllerTests: XCTestCase {
 
     func testViewDidload_requestedAuthorizationSuccessfully() throws {
-        let getplaceDataModel: GetPlaceResponseModel = try fetchStubModel(fileName: "GetPlace_London")
-        let sut = PlaceSearchViewController(PlaceSearchViewModel(SuccessdingFoursquareRepositoryStub(getplaceDataModel)), locationManager: SuccessdingMockLocationManager())
+        let sut = try makeSUT()
         XCTAssertEqual(sut.locationManager?.location?.coordinate.longitude, -0.1337)
         XCTAssertEqual(sut.locationManager?.location?.coordinate.latitude, 51.50998)
+    }
+
+    func testViewDidload_() throws {
+        let sut = try makeSUT()
+        let expectation = self.expectation(description: "fetchData")
+        _ = sut.view
+        sut.fetchData() { result in
+            switch result {
+            case .success:
+                expectation.fulfill()
+            case .failure(let error):
+                XCTFail("Fetch data fail with error: \(error.message)")
+            }
+        }
+
+        wait(for: [expectation], timeout: 3.0)
+        XCTAssertEqual(sut.tableView.numberOfRows(inSection: 0), 10)
     }
 
     func testViewDidload_requestedAuthorizationFailure() throws {
@@ -23,7 +39,13 @@ class PlaceSearchViewControllerTests: XCTestCase {
         let sut = PlaceSearchViewController(PlaceSearchViewModel(SuccessdingFoursquareRepositoryStub(getplaceDataModel)), locationManager: FailingMockLocationManager())
         XCTAssertEqual(sut.locationManager?.location, nil)
     }
-    
+
+    private func makeSUT() throws -> PlaceSearchViewController {
+        let getplaceDataModel: GetPlaceResponseModel = try fetchStubModel(fileName: "GetPlace_London")
+        let sut = PlaceSearchViewController(PlaceSearchViewModel(SuccessdingFoursquareRepositoryStub(getplaceDataModel)), locationManager: SuccessdingMockLocationManager())
+        return sut
+    }
+
     class SuccessdingMockLocationManager: LocationManager {
 
         func requestLocation() {}
@@ -42,4 +64,12 @@ class PlaceSearchViewControllerTests: XCTestCase {
         var delegate: CLLocationManagerDelegate?
         var location: CLLocation? = nil
     }
+}
+
+private extension UITableView {
+
+    func placeSearchCell(at row: Int) -> PlaceSearchTableViewCell? {
+        return dataSource?.tableView(self, cellForRowAt: IndexPath(row: row, section: 0)) as? PlaceSearchTableViewCell
+    }
+
 }
