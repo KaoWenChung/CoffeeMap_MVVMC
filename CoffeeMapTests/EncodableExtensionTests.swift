eb6f02dec1d1da15c5b05de8946df2cb532b7ed1
KaoWenChung <k022298@gmail.com>
2022-09-02 11:35:01 +0800

    Created london's GetPlaceParamModel data to test encodable method dictionary()

--- CoffeeMapTests/EncodableExtensionTests.swift
+++ CoffeeMapTests/EncodableExtensionTests.swift
@@ -10,7 +10,14 @@ import CoreLocation
 @testable import CoffeeMap
 
 class EncodableExtensionTests: XCTestCase {
-    func testDictionary() throws {
-        
+
+    func testDictionary_londonParameters() throws {
+        let sut = GetPlaceParamModel(ll: "51.50998,-0.1337", radius: 200, query: "coffee")
+        let result = sut.dictionary
+        XCTAssertEqual(result?.count, 3)
+        XCTAssertEqual(result?["ll"] as? String, "51.50998,-0.1337")
+        XCTAssertEqual(result?["radius"] as? Int, 200)
+        XCTAssertEqual(result?["query"] as? String, "coffee")
     }
+
 }
