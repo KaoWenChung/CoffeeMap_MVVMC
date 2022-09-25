f123f8642604d56fe163a9c5c934cb9e4ca79b9b
wyn <k022298@gmail.com>
2022-09-25 11:48:00 +0100

    Added Place detail view

---
+++ CoffeeMap/View/PlaceDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PlaceDetailViewController.swift
+//  CoffeeMap
+//
+//  Created by wyn on 2022/9/25.
+//
+
+import UIKit
+
+class PlaceDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
