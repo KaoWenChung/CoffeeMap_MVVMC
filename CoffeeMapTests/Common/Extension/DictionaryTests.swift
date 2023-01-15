251c2706d0ec84348a40b40dac0267e484d1336c
wyn <k022298@gmail.com>
2023-01-15 11:01:01 +0000

    Add DictionaryTests

---
+++ CoffeeMapTests/Common/Extension/DictionaryTests.swift
@@ -0,0 +1,21 @@
+//
+//  DictionaryTests.swift
+//  CoffeeMapTests
+//
+//  Created by wyn on 2023/1/15.
+//
+
+import XCTest
+@testable import CoffeeMap
+
+final class DictionaryTests: XCTestCase {
+    func testQueryString_intStringDouble() throws {
+        let test: [String : Any] = ["int": 0,
+                    "string": "String",
+                    "double": 0.0001]
+        let sut = test.queryString
+        XCTAssertTrue(sut.contains("double=0.0001"))
+        XCTAssertTrue(sut.contains("int=0"))
+        XCTAssertTrue(sut.contains("string=String"))
+    }
+}
